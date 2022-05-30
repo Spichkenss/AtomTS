994f2b8480b71215097da7506a166b29e7c370e6
Spichkenss <spichkens.2002@mail.ru>
2022-05-30 22:39:56 +0300

    .

--- hooks/useRelation.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-import { useSelector } from 'react-redux'
-import { selectCurrentRelation } from '../store/reducers/RelationSlice'
-
-export const useRelation = () => {
-	const relation = useSelector(selectCurrentRelation)
-
-	return useMemo(() => ({ relation }), [relation])
-}
